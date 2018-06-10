c55038eb4a59c3c496f1c636c19b08dd1716a5ca
Dinesh <dineshbabu.thoota@hotmail.com>
2018-06-10 12:18:27 +0530

    sample calculator

---
+++ ippcalc.js
@@ -0,0 +1,28 @@
+var menuData = {
+  homeMenu: {
+    templateId: '#home-screen',
+    elementClassName: '.home-screen',
+    templatePath: '_/media/b7d6e90ffbee4435abafac4849ad16e9.ashx'
+  },
+  benefitsMenu: {
+    templateId: '#select-benefits-screen',
+    elementClassName: '.select-benefits-screen',
+    templatePath: '_/media/36CB6480FFC846689443E96FA631DAE9.ashx'
+  },
+  coverMenu: {
+    templateId: '#cover-screen',
+    elementClassName: '.cover-screen',
+    templatePath: '_/media/DA551E72D25641A48A9047982A0DC2B6.ashx'
+  },
+  calculateBmiMenu: {
+    templateId: '#calculate-bmi-screen',
+    elementClassName: '.calculate-bmi-screen',
+    templatePath: '_/media/7B0595F35E414E93AD1BB1147F37FD5D.ashx'
+  },
+  resultsMenu: {
+    templateId: '#results-screen',
+    elementClassName: '.results-screen',
+    templatePath: '_/media/24A9CF2B77C843FBA9DAE43B72ECB872.ashx'
+  }
+};
+
